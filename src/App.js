43477f962df3129a64f6fb0742b5df50ea10859e
the-good-boy <shivam.awasthi702@gmail.com>
2020-08-17 18:25:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import {NotFound} from './components/pages/NotFound';
+import {Home} from './components/pages/Home';
+import Alert from './components/layout/alert';
+import About from './components/pages/About';
+import User from './components/Users/User';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+import './App.css';
+
+
+
+const App =()=>{
+ 
+    return (
+      <GithubState>
+        <AlertState>
+        <Router>
+        <div className="App">
+       <Navbar  />
+       <div className="container">
+         <Alert />
+         <Switch>
+           <Route exact path='/' component={Home} />
+
+          <Route exact path='/about' component={About} />
+          <Route exact path='/user/:login' 
+            component={User}
+          />
+          <Route component={NotFound} />
+         </Switch>
+        
+       </div>
+      
+      </div>
+      </Router>
+        </AlertState>
+         
+      </GithubState>
+     
+      );
+  
+
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react'
+import Search from '../Users/Search';
+import Users from '../Users/Users';
+
+
+export const Home = () => {
+    return (
+        <Fragment>
+            <Search />
+            <Users />
+        </Fragment>
+    )
+}
